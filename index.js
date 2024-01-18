d3d4fbd43bdd53f0fedea012e4a99b0325034a8f
Paweł Świercz <150139537+Pawel-Swiercz02@users.noreply.github.com>
2024-01-18 12:51:14 +0100

    Add files via upload
    Added the grid - canvas for painting, added function that checks if the mouse is held down, enabled painting

---
+++ index.js
@@ -0,0 +1,69 @@
+const grid = document.querySelector('.grid-div');
+const gridSqrs = [];
+let mouseDown = false;
+
+function isMouseDown() {
+    grid.addEventListener('mousedown', () => {
+        mouseDown = true;
+    });
+    grid.addEventListener('mouseup', () => {
+        mouseDown = false;
+    });
+};
+isMouseDown();
+
+function enablePainting(square) {
+    /*Paints the square if the mouse is down. For some reason, this doesn't work in two cases:
+    -if i click on a single square and release mouse
+    -if i hold down mouse, all squares get painted except the first one (the one I clicked on)
+    Code below the NEXT COMMENT fixes this.*/
+    square.addEventListener('mouseover', () => {
+        if (mouseDown === true) {
+            square.classList.add('painted');
+        }
+    });
+    /*This code fixes the two problems I indicated above.*/
+    square.addEventListener('mousedown', () => {
+        square.classList.add('painted');
+    });
+};
+
+function createGrid() {
+    for (let i = 0; i < 256; i++) {
+        const gridSquare = document.createElement('div');
+        grid.appendChild(gridSquare);
+        gridSquare.setAttribute('id', i + 1)
+        gridSquare.classList.add('grid-square');
+        gridSqrs.push(gridSquare);
+
+        enablePainting(gridSquare);
+    };
+};
+createGrid();
+
+
+function giveSquaresBackground() {
+/*Give squares different backgrounds to seperate them (I'm pretty much making a chess board pattern here)*/
+    for (let i = 0; i < gridSqrs.length; i++) {
+        if (i % 32 === 0) {
+            gridSqrs[i].classList.add('square-bg-1');
+            for (let j = i; j < i + 15; j++) {
+                if (gridSqrs[j].classList.contains('square-bg-1')) {
+                    gridSqrs[j + 1].classList.add('square-bg-2');
+                } else if (gridSqrs[j].classList.contains('square-bg-2')) {
+                    gridSqrs[j + 1].classList.add('square-bg-1');
+                };
+            };
+        } else if (i % 16 === 0 && i % 32 !== 0) {
+            gridSqrs[i].classList.add('square-bg-2');
+            for (let j = i; j < i + 15; j++) {
+                if (gridSqrs[j].classList.contains('square-bg-2')) {
+                    gridSqrs[j + 1].classList.add('square-bg-1');
+                } else if (gridSqrs[j].classList.contains('square-bg-1')) {
+                    gridSqrs[j + 1].classList.add('square-bg-2');
+                };
+            };
+        };
+    };
+};
+giveSquaresBackground();
